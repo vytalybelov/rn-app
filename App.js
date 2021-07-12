2f77afc4a396edcee1e8e4d997e62fe75059d4b5
Vitaliy S Belov <vitaliysbelov@gmail.com>
2021-07-12 12:05:56 +0300

    Initial commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header: {
+    fontSize: 32,
+    color: '#911',
+  }
 });
